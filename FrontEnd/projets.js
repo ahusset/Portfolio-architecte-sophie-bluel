f10d3cdcb13eaf843315076f427a0ecb74fbc8f1
ahusset <alexandre@husset.fr>
2023-05-26 11:43:33 +0200

    intégration des filtres

--- FrontEnd/projets.js
+++ FrontEnd/projets.js
@@ -1,29 +1,64 @@
+function createFigureElements(data) {
+    const galleryContainer = document.querySelector('.gallery');
+    galleryContainer.innerHTML = ''; // Effacer le contenu précédent
+
+    data.forEach(element => {
+        const imageElement = document.createElement('img');
+        imageElement.src = element.imageUrl;
+        imageElement.alt = element.title;
+
+        const figcaptionElement = document.createElement('figcaption');
+        figcaptionElement.textContent = element.title;
+
+        const figureElement = document.createElement('figure');
+        figureElement.appendChild(imageElement);
+        figureElement.appendChild(figcaptionElement);
+
+        galleryContainer.appendChild(figureElement);
+    });
+}
+
+function filterDataByCategoryId(data, categoryId) {
+    if (categoryId === 0) {
+        return data; // Renvoyer toutes les données si la catégorie est égale à 0
+    } else {
+        return data.filter(element => element.category.id === categoryId);
+    }
+}
+
+// Appel à fetch pour récupérer les éléments
 fetch('http://localhost:5678/api/works')
     .then(response => response.json())
     .then(data => {
-        // Sélection du conteneur <div> avec la classe "gallery"
-        const galleryContainer = document.querySelector('.gallery');
+        const uniqueCategories = data.reduce((acc, element) => {
+            const categoryId = element.category.id;
+            const existingCategory = acc.find(category => category.id === categoryId);
 
-        // Boucle sur tous les éléments du tableau de résultats
-        data.forEach(element => {
-            // Création de l'élément <img> avec attribut 'alt'
-            const imageElement = document.createElement('img');
-            imageElement.src = element.imageUrl;
-            imageElement.alt = element.title;
+            if (!existingCategory) {
+                acc.push(element.category);
+            }
 
-            // Création de l'élément <figcaption>
-            const figcaptionElement = document.createElement('figcaption');
-            figcaptionElement.textContent = element.title;
+            return acc;
+        }, []);
+        // Récupérer les catégories uniques à partir des éléments de works
+        console.log(uniqueCategories);
+        const allButton = document.createElement('button');
+        allButton.textContent = 'Tous';
+        allButton.addEventListener('click', () => {
+            createFigureElements(data); // Afficher toutes les catégories
+        });
+        document.querySelector('.category-buttons').appendChild(allButton);
 
-            // Création de l'élément <figure> pour contenir l'image et la légende
-            const figureElement = document.createElement('figure');
-            figureElement.appendChild(imageElement);
-            figureElement.appendChild(figcaptionElement);
-
-            // Ajouter l'élément <figure> au conteneur <div> avec la classe "gallery"
-            galleryContainer.appendChild(figureElement);
+        uniqueCategories.forEach(category => {
+            const button = document.createElement('button');
+            button.textContent = category.name;
+            button.addEventListener('click', () => {
+                const filteredData = filterDataByCategoryId(data, category.id);
+                createFigureElements(filteredData); // Afficher les éléments filtrés par catégorie
+            });
+            document.querySelector('.category-buttons').appendChild(button);
         });
     })
     .catch(error => {
-        console.error('Une erreur s\'est produite :', error);
+        console.error('Une erreur s\'est produite lors de la récupération des éléments :', error);
     });
